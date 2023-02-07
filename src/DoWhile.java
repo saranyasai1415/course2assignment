0455a0822c1f59783f2a1cd251deaf812c5651fe
saranyasai1415 <saranyasai1415@gmail.com>
2023-02-07 05:18:21 +0000

    program on do while

---
+++ src/DoWhile.java
@@ -0,0 +1,15 @@
+// section 5 --> 1.8
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		
+		do {
+			System.out.println("inside do loop");
+			i++;
+		}while(i<=10);
+
+	}
+
+}
