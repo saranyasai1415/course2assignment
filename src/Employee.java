4ffce5b0e296c393d4c90c93621bd3f40d5bf981
saranyasai1415 <saranyasai1415@gmail.com>
2023-02-07 05:35:31 +0000

    employee code comit

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+//section 5 --> 1.11
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ClassObjectExample e1 = new ClassObjectExample("nick",53,"Manager");
+		e1.printDetails();
+		ClassObjectExample e2 = new ClassObjectExample("sara",69,"consultant");
+		e2.printDetails();
+	}
+
+}
