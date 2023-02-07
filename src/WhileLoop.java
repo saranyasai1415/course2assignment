36339445741a68bf005b962dbf6dfa7cf7d32621
saranyasai1415 <saranyasai1415@gmail.com>
2023-02-07 05:52:06 +0000

    while loop commit

---
+++ src/WhileLoop.java
@@ -0,0 +1,16 @@
+
+// section 5 --> 1.7
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while(i<=10) {
+			System.out.println("i : "+i);
+			i++;
+		}
+			
+	}
+
+}
